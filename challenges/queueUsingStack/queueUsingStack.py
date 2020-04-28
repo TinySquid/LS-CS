e145f277ec7fd3574a3563a17e0082b785b5acca
Michael Nunes <tinysquid.git@gmail.com>
2020-04-28 10:54:37 -0700

    Add queueUsingStack challenge

---
+++ challenges/queueUsingStack/queueUsingStack.py
@@ -0,0 +1,62 @@
+"""
+Implement the following operations of a queue using stacks.
+
+    push(x) -- Push element x to the back of queue.
+    pop() -- Removes the element from in front of queue.
+    peek() -- Get the front element.
+    empty() -- Return whether the queue is empty.
+"""
+"""
+    Example:
+
+    MyQueue queue = new MyQueue();
+
+    queue.push(1);
+    queue.push(2);  
+    queue.peek();  // returns 1
+    queue.pop();   // returns 1
+    queue.empty(); // returns false
+"""
+
+
+class MyQueue:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack.insert(len(self.stack), x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.stack.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
